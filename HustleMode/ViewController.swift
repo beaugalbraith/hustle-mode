495369c26a3611639ec0417638c503df350db0f6
Beau <beaugalbraith@gmail.com>
2017-07-14 16:15:18 -0500

    Completed, maybe.

--- HustleMode/ViewController.swift
+++ HustleMode/ViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    @IBOutlet weak var darkBlueBackground: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var rocketImage: UIImageView!
+    @IBOutlet weak var cloudHolder: UIView!
+    
+    @IBOutlet weak var cloudsImage: UIImageView!
+    @IBOutlet weak var hustleLabel: UILabel!
+    @IBOutlet weak var onOffLabel: UILabel!
+    @IBAction func powerSwitched(_ sender: UIButton) {
+        cloudHolder.isHidden = false
+        darkBlueBackground.isHidden = true
+        powerButton.isHidden = true
+        audioPlayer.play()
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocketImage.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+            
+        }) {(finished) in
+            self.hustleLabel.isHidden = false
+            self.onOffLabel.isHidden = false
+        }
+    }
+    
+    var audioPlayer: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
